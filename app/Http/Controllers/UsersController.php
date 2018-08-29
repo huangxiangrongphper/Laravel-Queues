eb3a9b890303543e66e67287fa3b63a57c291e4d
Flourishing827 <huangxiangrong827@163.com>
2018-08-30 00:48:48 +0800

    Laravel队列 数据库驱动

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendReminderEmail;
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UsersController extends Controller
+{
+    public function store()
+    {
+        $users = User::where('id','>',24)->get();
+
+        foreach ($users as  $user) {
+            $this->dispatch(new SendReminderEmail($user));
+        }
+
+        return 'Done';
+    }
+}
---
+++ app/Jobs/SendReminderEmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\user;
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendReminderEmail extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        \Log::info('send remind email to '.$this->user->email);
+    }
+}
